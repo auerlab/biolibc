30e57eabbdda58848d7186f8909366535bd9f229
Jason Bacon <bacon@coral.acadix.biz>
2020-04-25 09:48:56 -0500

    Factor strptrcmp() out from vcf-split

--- biostring.h
+++ biostring.h
@@ -4,4 +4,7 @@
 /* chromosome-name-cmp.c */
 int chromosome_name_cmp(const char *n1, const char *n2);
 
+/* strptrcmp.c */
+int strptrcmp(const char **p1, const char **p2);
+
 #endif // __biostring_h__
