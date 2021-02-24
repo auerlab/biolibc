224b3bdf470aa420f4b706c2e15df9c9eaf7eabb
Jason Bacon <bacon@barracuda.uits.uwm.edu>
2021-02-24 12:40:07 -0600

    Add mdsv*() to extract fields separated by multiple possible delimiters

--- dsvio.c
+++ dsvio.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sysexits.h>
 #include "dsvio.h"
 
 /***************************************************************************
  *  Description:
- *      Read next tab-separated field
- *      Return delimiter ending the field (should be tab or newline)
+ *      Read next delim-separated field
+ *      Return delimiter ending the field, possibly newline or EOF
+ *      This function is separate from mdsv_read_field() for performance
  *
  *  History: 
  *  Date        Name        Modification
@@ -43,7 +45,9 @@ int     dsv_read_field(FILE *infile, char buff[], size_t buff_size,
 
 /***************************************************************************
  *  Description:
- *      Discard next field
+ *      Discard next field separated by the character delim
+ *      Return the delimiter encountered, possibly newline or EOF
+ *      This function is separate from mdsv_skip_field() for performance
  *
  *  History: 
  *  Date        Name        Modification
@@ -78,6 +82,69 @@ int     dsv_skip_rest_of_line(FILE *infile)
     
     while ( ((ch = getc(infile)) != EOF) && (ch != '\n') )
 	;
+    return ch;
+}
+
+
+/***************************************************************************
+ *  Description:
+ *      Read next multiple-delim-separated field
+ *      Fields may be separated by any character in the string delim
+ *      Return delimiter ending the field (member of delim or newline)
+ *
+ *  History: 
+ *  Date        Name        Modification
+ *  2021-02-24  Jason Bacon Begin
+ ***************************************************************************/
+
+int     mdsv_read_field(FILE *infile, char buff[], size_t buff_size,
+		       char *delim, size_t *len)
+
+{
+    size_t  c;
+    char    *p;
+    int     ch;
+    
+    for (c = 0, p = buff; (c < buff_size) && 
+			  ( strchr(delim, ch = getc(infile)) == NULL) &&
+			  (ch != '\n') && (ch != EOF); ++c, ++p )
+	*p = ch;
+    *p = '\0';
+    
+    if ( c == buff_size )
+    {
+	fprintf(stderr, "dsv_read_field(): Buffer overflow reading field.\n");
+	fprintf(stderr, "Buffer size = %zu\n", buff_size);
+	fputs(buff, stderr);
+	// FIXME: Replace this with another sentinal value?
+	// Would require all callers to handle both EOF and overflow
+	return EOF;
+    }
+    
+    *len = c;
+    return ch;
+}
+
+
+/***************************************************************************
+ *  Description:
+ *      Discard next field separated by any character in the string delim
+ *      Return the delimiter encountered, possibly newline or EOF
+ *
+ *  History: 
+ *  Date        Name        Modification
+ *  2021-02-24  Jason Bacon Begin
+ ***************************************************************************/
+
+int     mdsv_skip_field(FILE *infile, char *delim)
+
+{
+    int     ch;
+    
+    while ( (strchr(delim, ch = getc(infile)) == NULL) &&
+	    (ch != '\n') && (ch != EOF) )
+	;
+    
     return ch;
 }
 
--- dsvio.h
+++ dsvio.h
@@ -5,6 +5,9 @@
 int dsv_read_field(FILE *infile, char buff[], size_t buff_size,
 		   int delim, size_t *len);
 int dsv_skip_field(FILE *infile, int delim);
+int mdsv_read_field(FILE *infile, char buff[], size_t buff_size,
+		   char *delim, size_t *len);
+int mdsv_skip_field(FILE *infile, char *delim);
 int dsv_skip_rest_of_line(FILE *infile);
 int tsv_read_field(FILE *infile, char buff[], size_t buff_size,
 		   size_t *len);
