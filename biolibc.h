63c3057ab728091d9ece1d765b47b20e4b8c0286
Jason Bacon <bacon@moray.acadix.biz>
2021-07-19 11:28:27 -0500

    Clean up headers

--- biolibc.h
+++ biolibc.h
@@ -13,24 +13,24 @@
 #include <inttypes.h>       // PRIu64
 #endif
 
-#define BL_READ_OK                 0
-#define BL_READ_EOF                -1
-#define BL_READ_OVERFLOW           -2
-#define BL_READ_TRUNCATED          -3
-#define BL_READ_BL_GFF_TERMINATOR     -4
-#define BL_READ_EXTRA_COLS         -5
-#define BL_READ_MISMATCH           -6
+#define BL_READ_OK              0
+#define BL_READ_EOF             -1
+#define BL_READ_OVERFLOW        -2
+#define BL_READ_TRUNCATED       -3
+#define BL_READ_GFF_TERMINATOR  -4
+#define BL_READ_EXTRA_COLS      -5
+#define BL_READ_MISMATCH        -6
 
-#define BL_WRITE_OK                0
-#define BL_WRITE_FAILURE           -1
+#define BL_WRITE_OK             0
+#define BL_WRITE_FAILURE        -1
 
-#define BL_DATA_OK                 0
-#define BL_DATA_INVALID            -1      // Catch-all for non-specific error
-#define BL_DATA_OUT_OF_RANGE       -2
+#define BL_DATA_OK              0
+#define BL_DATA_INVALID         -1      // Catch-all for non-specific error
+#define BL_DATA_OUT_OF_RANGE    -2
 
-#define BL_CHROM_MAX_CHARS    256
-#define BL_POSITION_MAX_DIGITS     32
+#define BL_CHROM_MAX_CHARS      256
+#define BL_POSITION_MAX_DIGITS  32
 
-#define BL_CMD_MAX                 4096    // Arbitrary
+#define BL_CMD_MAX              4096    // Arbitrary
 
 #endif  // _biolibc_h_
