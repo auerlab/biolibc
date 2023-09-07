c3bd669e4a7b2151cfe939f0367c8f52a93e0764
Jason W. Bacon <bacon4000@gmail.com>
2023-09-07 17:23:38 -0500

    function-list.sh: Mention accessors and mutators

--- function-list.sh
+++ function-list.sh
@@ -7,6 +7,11 @@ Each function below is documented by a man page.  To view the documentation,
 install biolibc using your chosen package manager and run \`man function\`
 (e.g. \`man bl_fastq_read\`).
 
+This list does not include the numerous accessor and mutator functions
+and macros available for classes (bl_sam_t, bl_gff3_t, etc.).  See
+\$PREFIX/include/biolibc/\*-accessors.h and
+\$PREFIX/include/biolibc/\*-mutators.h for a current list.
+
 | Function | Purpose |
 |----------|---------|
 EOM
